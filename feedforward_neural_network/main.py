b7ac7b9eb49e611b53b4e67fcb71b598fcbcfa7e
高扬 <77232517@QQ.COM>
2019-10-31 16:21:15 +0800

    Update main.py

--- feedforward_neural_network/main.py
+++ feedforward_neural_network/main.py
@@ -91,3 +91,4 @@ with torch.no_grad():
     print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
 
 # Save the model checkpoint
+torch.save(model.state_dict(), 'model.ckpt')
